efa6e59c66119a3bb30c47d399895bb7019ed193
Jack Johnson <jrj221@byu.edu>
2025-11-05 22:23:12 -0700

    did getting started and started REPL

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,28 @@
 import chess.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        System.out.println("♕ 240 Chess Client ♕");
+        var state = "LOGGED_OUT";
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.printf("[%s] >>> ", state);
+            var input = scanner.nextLine();
+            switch (input) {
+                case "help":
+                    System.out.println("\tregister <USERNAME> <EMAIL> <PASSWORD> | Register a new user\n" +
+                            "\tlogin <USERNAME> <PASSWORD> | Login to an existing user\n" +
+                            "\tquit | Quit the chess program\n" +
+                            "\thelp | See possible commands");
+                    break;
+                case "quit":
+                    return;
+                case "login":
+                    break;
+                case "register":
+                    break;
+            }
+        }
     }
 }
