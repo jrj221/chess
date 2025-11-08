badeea560fb14283751c2f55874114689cfe8736
Jack Johnson <jrj221@byu.edu>
2025-11-08 11:37:51 -0700

    moved switch case login into separate functions. this means i declare client and requests/responses multiple times, but its worth the organization

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -8,6 +8,7 @@ import java.net.http.HttpResponse;
 import java.util.Map;
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("♕ 240 Chess Client ♕");
@@ -16,48 +17,79 @@ public class Main {
         while (true) {
             System.out.printf("[%s] >>> ", state);
             var input_words = scanner.nextLine().split(" ");
-            HttpClient client = HttpClient.newHttpClient(); // allows us to call endpoints
-            HttpRequest request;
-            HttpResponse<String> response;
             switch (input_words[0]) {
-                case "clear": // FOR TESTING ONLY, REMOVE BEFORE COMPLETION
-                    request = HttpRequest.newBuilder()
-                        .uri(new URI("http://localhost:8080/db"))
-                        .DELETE()
-                        .build();
-                    response = client.send(request, HttpResponse.BodyHandlers.ofString());
-                    System.out.println(response.body());
+                case "clear": { // FOR TESTING ONLY, REMOVE BEFORE COMPLETION
+                    clear();
                     break;
-                case "help":
-                    System.out.println("\tregister <USERNAME> <EMAIL> <PASSWORD> | Register a new user\n" +
-                            "\tlogin <USERNAME> <PASSWORD> | Login to an existing user\n" +
-                            "\tquit | Quit the chess program\n" +
-                            "\thelp | See possible commands");
+                } case "help": {
+                    System.out.println("""
+                            \tregister <USERNAME> <EMAIL> <PASSWORD> | Register a new user
+                            \tlogin <USERNAME> <PASSWORD> | Login to an existing user
+                            \tquit | Quit the chess program
+                            \thelp | See possible commands""");
                     break;
-                case "quit":
+                } case "quit": {
                     return;
-                case "login":
-                    if (input_words.length != 4) {
-                        System.out.println("login requires 3 arguments: USERNAME, EMAIL, and PASSWORD");
-                        break;
-                    }
-                    var username = input_words[1];
-                    var email = input_words[2];
-                    var password = input_words[3];
-                    var body = Map.of("username", username, "email", email, "password", password);
-                    var jsonBody = new Gson().toJson(body);
-                    request = HttpRequest.newBuilder()
-                        .uri(new URI("http://localhost:8080/session")) // we need to grab the port being used
-                        .header("Content-Type", "application.json")
-                        .POST(HttpRequest.BodyPublishers.ofString(jsonBody))
-                        .build();
-                    response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-                    System.out.println(response.body());
+                } case "login": {
+                    login(input_words);
                     break;
-                case "register":
+                } case "register": {
+                    register(input_words);
                     break;
+                }
             }
         }
+    }
+
+    public static void clear() throws Exception {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI("http://localhost:8080/db"))
+                .DELETE()
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+
+    public static void login(String[] input_words) throws Exception {
+        if (input_words.length != 4) {
+            System.out.println("logging in requires 3 arguments: USERNAME, EMAIL, and PASSWORD");
+            return;
+        }
+        HttpClient client = HttpClient.newHttpClient();
+        var username = input_words[1];
+        var email = input_words[2];
+        var password = input_words[3];
+        var body = Map.of("username", username, "email", email, "password", password);
+        var jsonBody = new Gson().toJson(body);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI("http://localhost:8080/session")) // we need to grab the port being used
+                .header("Content-Type", "application.json")
+                .POST(HttpRequest.BodyPublishers.ofString(jsonBody))
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+    }
+
+    public static void register(String[] input_words) throws Exception {
+        if (input_words.length != 4) {
+            System.out.println("registering requires 3 arguments: USERNAME, EMAIL, and PASSWORD");
+            return;
+        }
+        HttpClient client = HttpClient.newHttpClient();
+        // a lot of similar stuff with login, should it be refactored somehow?
+        var username = input_words[1];
+        var email = input_words[2];
+        var password = input_words[3];
+        var body = Map.of("username", username, "email", email, "password", password);
+        var jsonBody = new Gson().toJson(body);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI("http://localhost:8080/session")) // we need to grab the port being used
+                .header("Content-Type", "application.json")
+                .POST(HttpRequest.BodyPublishers.ofString(jsonBody))
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
     }
 }
