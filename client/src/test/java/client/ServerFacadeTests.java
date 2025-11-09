3aa3c34673c51581efa40bc39d645261c552816f
Jack Johnson <jrj221@byu.edu>
2025-11-08 17:22:10 -0700

    register good/bad tests made

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -5,18 +5,28 @@ import server.Server;
 import serverfacade.ServerFacade;
 
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class ServerFacadeTests {
 
     private static Server server;
     static ServerFacade facade;
 
     @BeforeAll
-    public static void init() {
+    public static void init() throws Exception {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
         facade = new ServerFacade(port);
+        facade.clear();
     }
+
+    @BeforeEach
+    public void clear() throws Exception {facade.clear();}
 
     @AfterAll
     static void stopServer() {
@@ -25,8 +35,23 @@ public class ServerFacadeTests {
 
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    public void registerSuccessful() throws Exception {
+        String[] input_words = {"register", "joe", "email", "pass"};
+        assertNull(facade.getAuthToken());
+        facade.register(input_words);
+        assertNotNull(facade.getAuthToken());
+    }
+
+    @Test
+    public void registerUsernameTaken() throws Exception {
+        String[] input_words = {"register", "joe", "email", "pass"};
+        facade.register(input_words);
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream(); // buffers stream that captures stdout
+        System.setOut(new PrintStream(out)); // redirects stdout to my buffer
+        facade.register(input_words); // attempt to register again
+        System.setOut(System.out); // MUST UNDO REDIRECTION so that output goes to console like it should
+        assertEquals("Username already taken, try a different one\n", out.toString());
     }
 
 }
